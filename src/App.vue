05a4400f790719dd6ed9785af4ec37c19235ad2b
C4terp1llar <m0skvitin@mail.ru>
2024-03-12 23:25:50 +0300

    основы composition api (ref, reactive), хелперы (isRef, isReactive), работа с моделями, $refs, $attrs. Работа с компонентами (эмиты, пропсы)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,111 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="card">
+      <h1>Vue Composition Api</h1>
+      <small>data, methods, computed, watch</small>
+
+      <div class="form-control">
+        <label for="reff">ref</label>
+        <input type="text" id="reff" ref="textInp">
+        <label for="name">Модель name</label>
+        <input type="text" id="name" v-model="fName">
+      </div>
+
+      <button class="btn" @click="change">Изменить</button>
+    </div>
+    <app-info
+      :name="name"
+      :version="version"
+      @change-to-ver3="changeVer"
+      class="test-from-app"
+    >
+     <template>footer</template>
+    </app-info>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref, reactive, computed, watch, toRefs, isRef, isReactive} from 'vue';
+import appInfo from "@/components/AppInfo.vue";
+
+// для проверки isRef, isReactive
+// вернет булевое значение
+// console.log(isRef(name)); || console.log(isReactive(name));
+
+/*
+  reactive
+  добавляет реактивность объекту (в случае работы с объектами)
+  в отличии от ref (который ссылается на refImpl в котором лежит прокси обджект)
+  reactive ссылается сразу на прокси обджект
+*/
+
 
 export default {
-  name: 'App',
+  setup(){
+    const name = ref('VueJS');
+    let version = ref(3);
+    const textInp = ref(null);
+    const fName = ref(null);
+
+    const doubleVersion = computed(() => {
+      return version.value * 2
+    })
+
+    // watch([doubleVersion, name], (newVals, oldVals) => {
+    //   console.log(`new doubleVersion: ${newVals[0]} old doubleVersion: ${oldVals[0]}`);
+    //   console.log(`new name: ${newVals[1]} old name: ${oldVals[1]}`);
+    // })
+
+    // const framework = reactive({
+    //   name: 'VueJS',
+    //   version: 3
+    // })
+  
+
+    function changeInfo(){
+      name.value = 'Vue JS!';
+      version.value = 4;
+
+      // console.log('реф на инпут: ', textInp.value);
+      // console.log('реф на значение инпута: ', textInp.value.value);
+      //
+      // console.log('модель: ', fName);
+
+      // framework.name = 'Vue JS!';
+      // framework.version = 4;
+    }
+
+    function changeVer (val) {
+      version.value = val;
+    }
+
+    return{
+      // С использованием ref (отдельно созданные данные)
+      name: name,
+      version: version,
+      doubleVersion: doubleVersion,
+      // textInp: textInp,
+      fName: fName,
+
+      // С использованием ref, объект с данными
+      // name: framework.value.name,
+      // version: framework.value.version,
+      // framework: framework,
+
+      // C использвоанием reactive (объект с данными)
+      // name: framework.name,
+      // version: framework.version,
+
+      // ...toRefs(framework),
+      // // или
+      // framework: framework,
+
+      change: changeInfo,
+      changeVer
+    }
+  },
   components: {
-    HelloWorld
+    appInfo,
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/AppInfo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card">
+    <p>Название: <strong>{{name}}</strong></p>
+    <p>Версия: {{version}} ({{doubleVersion}})<strong></strong></p>
+
+    <button class="btn warning" @click="changeVersion">На 3 версию</button>
+  </div>
+</template>
+
+<script >
+import {computed} from 'vue'
+
+/*
+
+для реботы с пропсами в композишн апи, нужно передавать их в параметре сетапу
+для ряботы с эмитами, слотами и атрсами используется context, который тоже передается в параметрах сетапу
+
+*/
+export default {
+  props: ['name', 'version'],
+  emits: ['changeToVer3'],
+  setup(props, context){
+    const doubleVersion = computed(() => {
+      return props.version * 2;
+    })
+
+    function change(){
+      context.emit('changeToVer3', 3);
+      console.log(context)
+    }
+
+    return{
+      doubleVersion,
+      changeVersion: change
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
